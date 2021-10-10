d6e59322e711ff5733ef679b59491d20080ba329
brinapaula <67146769+brinapaula@users.noreply.github.com>
2021-10-10 20:11:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+#Project Done by Franstan DSouza, Prajwal Acharya, Brina Paula, BindhuShree
+
+import student as st 
+import teacher as t
+import admin as ad
+import details as dt
+
+def main_function():
+    print("----------------Internal Assessment Score System-------------------")
+    data=["1.Login as Administrator 2.Login as Teacher 3.Login as Student"]
+    table =dt.get_pretty_table(data)
+    print(table)
+    choice=int(input("Choice:"))
+    if choice==1:
+        ad.main_function()
+    elif choice==2:
+        t.main_function()
+    elif choice==3:
+        st.main_function()
+    else:
+        print("Invalid Choice.")
+        exit()
+
+if __name__=="__main__":
+    main_function()
+ 
